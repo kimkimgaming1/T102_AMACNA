acf2bb2e5a05cb35085c328f2cf459d135b6acae
Khyle Amacna <89185129+kimkimgaming1@users.noreply.github.com>
2021-09-26 12:34:46 +0800

    Add files via upload

---
+++ qwe.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    system("COLOR 0A");
+    printf("\nMy name is Khyle Ivan Khim V. Amacna");
+    printf("\nI am 19 Years OLD");
+    return 0;
+}
