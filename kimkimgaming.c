d6ae45e4728a3d0bf53ac6c1963447d4b150e211
Khyle Amacna <89185129+kimkimgaming1@users.noreply.github.com>
2021-11-06 15:01:36 +0800

    Add files via upload

--- kimkimgaming.c
+++ kimkimgaming.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
 int main(){
-
 system("COLOR 0A");
-for(int i = 1; i < 10; i++){
-for(int e = i; e <= 9; e++){
-   printf("%d ", e);
-}
-printf("\n");
-}
 
+int n;
+
+printf("Input any integer:");
+scanf("%d", &n);
+while(n > 1){
+    if(n % 2 == 0){
+    n = n / 2;
+    printf("current given value is %d\n", n);
+}else{
+n = n* 3 + 1;
+printf("current given value is %d\n", n);
+}
+}
 return 0;
 }
